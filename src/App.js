64494832ab2ef4dc70a8012a61267ef6029f15f6
noizziday <danya.doulbekov@gmail.com>
2022-11-24 19:13:44 +0600

    deploy lesson 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MainRoutes from './MainRoutes';
+import Navbar from './components/Navbar/Navbar';
+import AuthContextProvider from './contexts/AuthContextProvider';
+import ProductContextProvider from './contexts/ProductContextProvider';
+import CartContextProvider from './contexts/CartContextProvider';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <CartContextProvider>
+        <ProductContextProvider>
+          <AuthContextProvider>
+            <Navbar />
+            <MainRoutes />
+          </AuthContextProvider>
+        </ProductContextProvider>
+      </CartContextProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
